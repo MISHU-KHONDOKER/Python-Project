0b8e541ceb037ddb49c04c7fcb27749100e4649a
MISHU-KHONDOKER <148381404+MISHU-KHONDOKER@users.noreply.github.com>
2023-10-22 15:01:37 +0800

    Add files via upload
    Band Name

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to Project:Band Name Generator")
+
+city=input("What is the city you grew up in?\n ")
+pet=input("What is your pet's name?\n ")
+print("Your Band Name Could be " + city + " " + pet)
