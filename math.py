0a4d7f10beb546c321129842da9d113977a5c7a4
MISHU-KHONDOKER <148381404+MISHU-KHONDOKER@users.noreply.github.com>
2023-11-08 01:49:32 +0800

    Add files via upload

---
+++ math.py
@@ -0,0 +1,13 @@
+#print(8/3)
+#print(int(8/3))
+#print(round(8/3))
+#print(round(8/3, 2))
+#print(type(4/2))
+
+age=input("What is your current age ?")
+#print(type(age))
+result=90-int(age)
+days=int(result)*365
+weeks=int(result)*52
+months=int(result)*12
+print(f"You have {days} days , {weeks} weeks, {months} months left")
