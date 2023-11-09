ee0bc3b7c0582f5c50bda7df29a9e1ea197d94e4
MISHU-KHONDOKER <148381404+MISHU-KHONDOKER@users.noreply.github.com>
2023-11-10 03:22:35 +0800

    Add files via upload

---
+++ Ticket Rollercoaster.py
@@ -0,0 +1,21 @@
+print("Welcome to Rollercoaster")
+height=int(input("What is your height in cm? "))
+bill = 0
+if height >= 120 :
+    print("You can ride the rollercoaster!!")
+    age=int(input("What is your age? "))
+    if age < 12 :
+        bill = 5
+        print("Child tickets are $5")
+    elif age <= 18 :
+        bill= 7
+        print("Youth tickets are $7")
+    else :
+        bill= 12
+        print("Adults tickets are $12")
+    photos=input("Do you want to take photos? Y or N ")
+    if photos == "Y":
+        bill += 3
+    print(f"Your final bill is $ {bill}")
+else :
+    print("You have grow taller to ride it!!")
