8840ab66ecc3d0416d6f5a4ad7544469b2c208b9
MISHU-KHONDOKER <148381404+MISHU-KHONDOKER@users.noreply.github.com>
2023-11-10 03:23:14 +0800

    Add files via upload

---
+++ Leap Year Final.py
@@ -0,0 +1,12 @@
+print("Welcome to Leap Year Calculation!!! ")
+year = int(input("Which year do you want to check? "))
+if year % 4 == 0 :
+    if year % 100 == 0 :
+        if year % 400 == 0 :
+            print(f"{year} is Leap Year")
+        else :
+            print(f"{year} is not a Leap Year")
+    else :
+        print(f"{year} is Leap Year")
+else:
+    print(f"{year} not a Leap Year")
